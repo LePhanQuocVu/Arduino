dcc7e97e20d258944efc55ae3f39e77fa13964f8
Quốc Vũ <138181674+LePhanQuocVu@users.noreply.github.com>
2025-07-25 18:08:47 +0700

    connect Mqqtt - readData from sensor - sendDataOnAdafruitIO

--- OTA_blinky.ino
+++ OTA_blinky.ino
@@ -3,14 +3,47 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+#include <DHT.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 
-#define LED_WIFI 2
-#define LED 18
+
+#define DHTPIN 14       // D4 = GPIO14
+#define DHTTYPE DHT11  // Type
+#define LED_WIFI 4
+
+unsigned long lastReadTime = 0;
+const unsigned long interVal = 3000;
+
+unsigned long lastSend = 0;
+const unsigned long sendInterval = 5000; // 5 giây
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float currentTemp = NAN;
+float currentHumi = NAN;
 
 
 const char* host = "esp32";
-const char* ssid = "Ho Hanh";
-const char* password = "0983616035";
+const char* ssid = "DATN";
+const char* password = "12345678";
+
+
+// Adafruit IO
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // Cổng MQTT không mã hoá
+#define AIO_USERNAME    "****"        // Thay bằng tài khoản của bạn
+#define AIO_KEY         "****"         // Thay bằng AIO Key của bạn
+
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+// Feed
+Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+
+Adafruit_MQTT_Publish humidityFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
 
 
 WebServer server(80);
@@ -101,13 +134,54 @@ const char* serverIndex =
  "</script>";
 
 /*
- * setup function
+ * Define function
  */
-void setup(void) {
- pinMode(LED_WIFI, OUTPUT);
- pinMode(LED,OUTPUT);
-  Serial.begin(9600);
-  // Connect to WiFi network
+
+
+void readDHT11() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Lỗi đọc từ cảm biến DHT11!");
+    return;
+  }
+
+  currentTemp = t;
+  currentHumi = h;
+
+  Serial.print("Nhiệt độ: ");
+  Serial.print(t);
+  Serial.print("°C  |  Độ ẩm: ");
+  Serial.print(h);
+  Serial.println("%");
+}
+
+void connectMQTT() {
+  Serial.print(F("Connecting to Adafruit IO... "));
+  int8_t ret;
+  while ((ret = mqtt.connect()) != 0) {
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if(ret >= 0)
+      mqtt.disconnect();
+
+    Serial.println(F("Retrying connection..."));
+    delay(10000);
+  }
+  Serial.println(F("Adafruit IO Connected!"));
+}
+
+void connectWifi() {
+// Connect to WiFi network
   WiFi.begin(ssid, password);
   Serial.println("");
 
@@ -125,7 +199,18 @@ void setup(void) {
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+}
 
+/*
+ * setup function
+ */
+void setup(void) {
+
+  Serial.begin(9600);
+   pinMode(LED_WIFI, OUTPUT);
+  dht.begin();
+  connectWifi();
+ 
   /*use mdns for host name resolution*/
   if (!MDNS.begin(host)) { //http://esp32.local
     Serial.println("Error setting up MDNS responder!");
@@ -163,10 +248,6 @@ void setup(void) {
     } else if (upload.status == UPLOAD_FILE_END) {
       if (Update.end(true)) { //true to set the size to the current progress
         Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-         digitalWrite(LED_WIFI,HIGH);
-          delay(300);
-          digitalWrite(LED_WIFI,LOW);
-          delay(300);
       } else {
         Update.printError(Serial);
       }
@@ -180,8 +261,39 @@ void setup(void) {
 void loop(void) {
   server.handleClient();
   delay(1);
- digitalWrite(LED,HIGH);
- delay(2000);
- digitalWrite(LED,LOW);
- delay(2000);
+  unsigned long currentTime = millis();
+  
+  // Đọc dữ liệu mỗi 3s
+  if(currentTime - lastReadTime >= interVal) {
+    readDHT11();
+    lastReadTime = currentTime;
+  }
+
+
+   // ping adafruit io a few times to make sure we remain connected
+  if(! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if(! mqtt.connected())
+      connectMQTT();
+  }
+
+    // Gửi dữ liệu mỗi 5s
+    if (currentTime - lastSend >= sendInterval) {
+       // ping adafruit io a few times to make sure we remain connected
+    if(! mqtt.ping(3)) {
+      // reconnect to adafruit io
+      if(! mqtt.connected())
+        connectMQTT();
+    }
+
+    if (!isnan(currentTemp) && !isnan(currentHumi)) {
+      Serial.println("Gửi dữ liệu lên Adafruit IO...");
+      temperatureFeed.publish(currentTemp);
+      humidityFeed.publish(currentHumi);
+    }
+
+    lastSend = currentTime;
+  }
+
+
 }
